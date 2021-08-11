e02aaccb0b43d44699d96b9e5414c2e39ab17362
warrenniu <warrenniu@gmail.com>
2021-08-11 14:31:17 -0400

    using SafeAreaView component

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
 
 export default function App() {
   console.log("App executed")
+  let x = 1;
+
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
+   
   },
 });

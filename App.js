d27c2ce7e0cb914a9826259961882725785ba65a
warrenniu <warrenniu@gmail.com>
2021-08-19 12:26:47 -0400

    Added Welcome Screen

--- App.js
+++ App.js
@@ -15,48 +15,58 @@ import {
    useDimensions, 
    useDeviceOrientation,
 } from '@react-native-community/hooks'
+import WelcomeScreen from './app/screen/WelcomeScreen';
 
 export default function App() {
-  const handlePress = () => console.log("Text pressed");
-  const {landscape} = useDeviceOrientation()
+  // const handlePress = () => console.log("Text pressed");
+  // const {landscape} = useDeviceOrientation()
 
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={{
-        backgroundColor: '#fff',
-        flex: 1,
-        flexDirection: 'row', //determines main (primary) and secondary (cross) axis
-        justifyContent: 'center', //main axis
-        alignItems: 'center', //secondary axis
-      }}
-      >
-        <View style={{
-          backgroundColor: 'gold',
-          width: 100,
-          height: 100
-        }}/>
-        <View style={{
-          backgroundColor: 'tomato',
-          width: 100,
-          height: 100
-        }}/>
-        <View style={{
-          backgroundColor: 'dodgerblue',
-          width: 100,
-          height: 100
-        }}/>
+  return <WelcomeScreen />
+    // <SafeAreaView style={styles.container}>
+    //   <View style={{
+    //     backgroundColor: '#fff',
+    //     flex: 1,
+    //     flexBasis: 100, //sets width or height (depending on primary axis)
+    //     flexDirection: 'row', //determines main (primary) and secondary (cross) axis
+    //     justifyContent: 'center', //main axis
+    //     alignItems: 'center', //secondary axis. determines content of a single line
+    //     alignContent: 'center', //determines content placement as a whole. No effect if there's no wrap
+    //     flexWrap: 'wrap'
+    //   }}
+    //   >
+    //     <View style={{
+    //       backgroundColor: 'gold',
+    //       width: 100,
+    //       height: 100,
+    //       alignSelf: 'flex-start' //aligns each individual item
+    //     }}/>
+    //     <View style={{
+    //       backgroundColor: 'tomato',
+    //       width: 100,
+    //       height: 100
+    //     }}/>
+    //     <View style={{
+    //       backgroundColor: 'dodgerblue',
+    //       width: 100,
+    //       height: 100
+    //     }}/>
+    //     <View style={{
+    //       backgroundColor: 'green',
+    //       width: 100,
+    //       height: 100
+    //     }}/>
         
-      </View>
-    </SafeAreaView>
-  );
+    //   </View>
+    // </SafeAreaView>
+  // );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     // justifyContent: 'center',
+//     // alignItems: 'center',
+//   },
+// });
---
+++ app/screen/WelcomeScreen.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Image, ImageBackground, StyleSheet, View, Text } from 'react-native';
+
+function WelcomeScreen(props) {
+    return (
+        <ImageBackground 
+            style={styles.background}
+            source={require('../assets/background.jpg')}
+        >
+            <View style={styles.logoContainer}>
+            <Image 
+                style={styles.image}
+                source={require('../assets/logo-red.png')} 
+            />
+            <Text>Sell what you don't need!</Text>
+            </View>
+            <View style={styles.loginButton}
+            >
+            </View>
+            <View style={styles.registerButton}
+            >
+            </View>
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    image: {
+        // alignSelf: 'flex-start',
+        height: 100,
+        width: 100,
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#fc5c65',
+    },
+    logoContainer: {
+        position: 'absolute',
+        top: 70,
+        alignItems: 'center',
+
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#4ecdc4',
+    },
+})
+
+export default WelcomeScreen;
